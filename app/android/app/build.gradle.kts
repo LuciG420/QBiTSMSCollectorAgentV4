e7c60155922bb15a42229d1ee436303cb6f0abba
Luci Odinson <luci@luciodinson.me>
2025-03-10 10:05:45 -0400

    build fixes

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,9 +29,9 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        manifestPlaceholders["appAuthRedirectScheme"] = "ink.qbit.app"  // <<-- CHECK!
-        manifestPlaceholders["auth0Domain"] = "dev-ukaaosck1wcq8dyt.us.auth0.com" 
-        manifestPlaceholders["auth0Scheme"] = "https"
+        manifestPlaceholders.put("appAuthRedirectScheme", "ink.qbit.app") // Correct!
+        manifestPlaceholders.put("auth0Domain", "dev-ukaaosck1wcq8dyt.us.auth0.com") 
+        manifestPlaceholders.put("auth0Scheme", "https")
     }
 
     buildTypes {
@@ -38,8 +39,32 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            // isMinifyEnabled = false
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    sourceSets {
+        getByName("main").java.srcDirs(listOf("src/main/kotlin"))
+    }    
+    dependencies {
+
+        implementation("androidx.core:core-ktx:1.8.0")
+        implementation("androidx.appcompat:appcompat:1.4.1")
+        implementation("com.google.android.material:material:1.5.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.3")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+        implementation("io.flutter:flutter_embedding:1.0.0")
+    }    
 }
 
 flutter {
--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -1,9 +1,25 @@
 allprojects {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
+    }
+}
+
+rootProject.name = "ink.qbit.app" // Replace with your app name
+
+//include(":app")
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
